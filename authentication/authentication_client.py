a54bca48a1070246d59a5d133ba9f0d6b3683f2c
JackOefnLad <qyrtchy@gmail.com>
2025-12-23 15:40:52 +0300

    API clients

---
+++ authentication/authentication_client.py
@@ -0,0 +1,18 @@
+from clients.api_client import APIClient 
+#папка -> файл -> класс
+from httpx import Response
+from typing import TypedDict
+
+class LoginRequestDict(TypedDict):
+    email: str
+    password: str
+class RefreshRequestDict(TypedDict):
+    refreshToken: str
+
+class AuthenticationClient(APIClient):
+    def login_api(self, request: LoginRequestDict)-> Response:
+        return self.post("/api/v1/authentication/login", json=request)
+    def refresh_api(self,request):
+        return self.post("/api/v1/authentication/refresh", json = request)
+# client = AuthenticationClient()
+# client.login_api({'email':'', 'password':''})
---
+++ httpx_client.py
@@ -0,0 +1,33 @@
+import httpx
+from tools.fakers import get_random_email
+
+create_user_payload = {
+    "email": get_random_email(),
+    "password": "string",
+    "lastName": "string",
+    "firstName": "string",
+    "middleName": "string"
+}
+
+create_user_response = httpx.post("http://localhost:8000/api/v1/users", json=create_user_payload)
+create_user_response_data = create_user_response.json()
+
+print("Creation data: ",create_user_response_data)
+
+login_payload = {
+    "email":create_user_payload["email"],
+    "password":create_user_payload["password"]
+}
+
+login_response = httpx.post("http://localhost:8000/api/v1/authentication/login", json=login_payload)
+login_response_data = login_response.json()
+print("Login data: ", login_response_data)
+
+client = httpx.Client(
+    base_url = "http://localhost:8000", 
+    timeout=100, 
+    headers={"Authorization":f"Bearer {login_response_data['token']['accessToken']}"} )
+
+get_user_me_response = client.get("/api/v1/users/me")
+get_user_me_response_data= get_user_me_response.json()
+
